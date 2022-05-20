76db92aca2c2a8c1a0f8af88eb0d2e34ef852cd6
hanwei-dev <69997301+hanwei-dev@users.noreply.github.com>
2022-05-20 22:09:38 +0800

    Update autoDeploy_Tomcat.sh

--- autoDeploy_Tomcat.sh
+++ autoDeploy_Tomcat.sh
@@ -1,3 +1,12 @@
+说明：
+1.新建一个目录autoDeploy与tomcat目录放在同级目录下
+　　(1)autoDeploy.sh放在autoDeploy目录下
+　　(2)需要部署的test.war放在autoDeploy目录下(test.war为需要部署的到tomcat上的war包)
+2.tomcatPath=tomcat-7（指定tomcat文件目录名称，tomcat-7改为自己tomcat的目录名称）
+3.执行autoDepoy.sh即可实现自动部署
+　　新建的autoDepoy.sh若没有执行权限，需要执行：chmod +x autoDeploy.sh
+
+
 #! /bin/sh
 echo '####################开始自动部署####################'
 path=`pwd` #当前路径
@@ -17,3 +26,18 @@ sleep 1 #休眠1s
 cd ../bin
 ./startup.sh #启动tomcat服务
 echo '####################部署结束####################'
+关停脚本stop.sh
+PID=`ps -ef | grep sms-2.0.jar | grep -v grep | awk '{print $2}'`
+if [ -z "$PID" ]
+then
+    echo Application is already stopped
+else
+    echo kill $PID
+    kill -9 $PID
+fi
+
+重启脚本run.sh
+echo stop application
+source stop.sh
+echo start application
+source start.sh
